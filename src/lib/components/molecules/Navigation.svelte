cc79cae0b4fc32a6326ddb08fa9f05ad838de715
Thibaut Flottat <thibaut.flottat@icloud.com>
2023-05-14 17:10:57 +0200

    none

--- src/lib/components/molecules/Navigation.svelte
+++ src/lib/components/molecules/Navigation.svelte
@@ -1,6 +1,6 @@
-<nav class="grow">
-	<ul>
-		<li><a href="/">Home</a></li>
+<nav class="grow pt-8">
+	<ul class="text-zinc-400 uppercase flex flex-col gap-8 tracking-widest">
+		<li><a href="/" class="hover:text-zinc-50 transition duration-300 hover:duration-300">Home</a></li>
 		<li><a href="/about">About</a></li>
 		<li><a href="/research">Research</a></li>
 		<li><a href="/photography">Photography</a></li>
