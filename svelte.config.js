3f4319dfa6aa4490e5656154d03d068465ed7db3
Thibaut Flottat <thibaut.flottat@icloud.com>
2024-01-14 14:38:26 +0100

    svelte-sitemap > super-sitemap

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-// import { mdsvex } from 'mdsvex';
-// import rehypeExternalLinks from 'rehype-external-links';
-// import rehypeSlug from 'rehype-slug';
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-// import remarkMath from 'remark-math';
-// import rehypeKatex from 'rehype-katex-svelte';
-
-const extensions = ['.svelte', '.md'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,33 +11,7 @@ const config = {
 			handleHttpError: 'warn'
 		}
 	},
-	preprocess: [
-		vitePreprocess()
-		// mdsvex({
-		// 	extensions: extensions,
-		// 	remarkPlugins: [remarkMath],
-		// 	rehypePlugins: [
-		// 		rehypeKatex,
-		// 		rehypeExternalLinks, // Adds 'target' and 'rel' to external links
-		// 		rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
-		// 		[
-		// 			// rehypeAutolinkHeadings,
-		// 			// {
-		// 			// 	// Adds hyperlinks to the headings, requires rehypeSlug
-		// 			// 	behavior: 'prepend',
-		// 			// 	properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
-		// 			// 	content: {
-		// 			// 		type: 'element',
-		// 			// 		tagName: 'span',
-		// 			// 		properties: {},
-		// 			// 		children: [{ type: 'text', value: '#' }]
-		// 			// 	}
-		// 			// }
-		// 		]
-		// 	]
-		// })
-	],
-	extensions: extensions
+	preprocess: [vitePreprocess()]
 };
 
 export default config;
